01553ccf6ece60e10c5e35a6b3ed76152e39f229
Ronan <ronan.bonnet42@wanadoo.fr>
2023-10-25 11:51:24 +0200

    Test GH Action workflow.

--- src/main/java/com/example/contactdiscovery/Main.java
+++ src/main/java/com/example/contactdiscovery/Main.java
@@ -119,6 +119,7 @@ public class Main {
         System.out.println(ronan.sendUsername("Ronan"));
         System.out.println(anna.sendUsername("Ronan"));
 
+
         anna.close();
         ronan.close();
     }
