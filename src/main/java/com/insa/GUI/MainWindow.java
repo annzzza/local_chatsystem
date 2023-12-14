85b3b67bdbeb438ff3d4943124d9f68da567a2aa
annzzza <annacazeneuve@gmail.com>
2023-12-14 10:38:46 +0100

    JavaFX GUI changed to Swing, JavaFX code remains in comments

--- src/main/java/com/insa/GUI/MainWindow.java
+++ src/main/java/com/insa/GUI/MainWindow.java
@@ -4,6 +4,96 @@ import com.insa.database.LocalDatabase;
 import com.insa.network.NetworkManager;
 import com.insa.network.User;
 import com.insa.utils.MyLogger;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainWindow {
+
+    private final JFrame window = new JFrame();
+    private final JPanel panel = new JPanel();
+    private final JLabel usernameLabel = new JLabel("Enter your username:");
+    private final JTextField inputUsername = new JTextField();
+    private final JButton enterButton = new JButton("Enter");
+    private final JLabel chooseOtherUsernameLabel = new JLabel();
+
+    public void start() {
+        window.setTitle("ODD");
+        window.setSize(400, 275);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.insets = new Insets(10, 10, 10, 10);
+
+        JLabel welcomeText = new JLabel("Welcome");
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 2;
+        panel.add(welcomeText, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.gridwidth = 1;
+        panel.add(usernameLabel, constraints);
+
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        inputUsername.setPreferredSize(new Dimension(100, 30));
+        panel.add(inputUsername, constraints);
+
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        buttonPanel.add(enterButton);
+        enterButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                enterButtonHandler();
+            }
+        });
+
+        constraints.gridx = 1;
+        constraints.gridy = 2;
+        panel.add(buttonPanel, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 4;
+        constraints.gridwidth = 2;
+        panel.add(chooseOtherUsernameLabel, constraints);
+
+        window.add(panel);
+        window.setVisible(true);
+    }
+
+    private void enterButtonHandler() {
+
+        LocalDatabase.Database.currentUser = new User(inputUsername.getText());
+
+        NetworkManager nwm = NetworkManager.getInstance();
+
+        MyLogger.getInstance().info("Begin client discovery");
+        
+        if(nwm.discoverNetwork(inputUsername.getText())) {
+
+            chooseOtherUsernameLabel.setForeground(Color.RED);
+            chooseOtherUsernameLabel.setText("Username not available, please choose a new one.");
+        } else {
+            chooseOtherUsernameLabel.setText("Connected");
+
+            SecondWindow secondWindow = new SecondWindow();
+            secondWindow.start();
+        };
+
+    }
+}
+
+
+/*
+import com.insa.database.LocalDatabase;
+import com.insa.network.NetworkManager;
+import com.insa.network.User;
+import com.insa.utils.MyLogger;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -97,3 +187,6 @@ public class MainWindow {
         };
     }
 }
+
+*/
+
--- src/main/java/com/insa/GUI/SecondWindow.java
+++ src/main/java/com/insa/GUI/SecondWindow.java
@@ -1,5 +1,87 @@
 package com.insa.GUI;
 
+
+import com.insa.database.LocalDatabase;
+import com.insa.network.ConnectedUser;
+import com.insa.network.NetworkManager;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SecondWindow {
+
+    private final JFrame window = new JFrame();
+    private final JPanel grid = new JPanel();
+    private final JTextField inputNewUsername = new JTextField();
+    private final JButton changeUsernameButton = new JButton("Change Username");
+    private final JLabel changedUsernameLabel = new JLabel("");
+
+    public void start() {
+        window.setTitle("Chats");
+        window.setSize(400, 275);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        grid.setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.insets = new Insets(10, 10, 10, 10);
+
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        inputNewUsername.setPreferredSize(new Dimension(100, 30));
+        grid.add(inputNewUsername, constraints);
+
+        JPanel buttonBox = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        buttonBox.add(changeUsernameButton);
+        changeUsernameButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                changeUsernameButtonHandler();
+            }
+        });
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        grid.add(buttonBox, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        grid.add(changedUsernameLabel, constraints);
+
+        window.add(grid);
+        window.setVisible(true);
+    }
+
+    private void changeUsernameButtonHandler() {
+        // Replace with your logic for handling the button click
+        NetworkManager nwm = NetworkManager.getInstance();
+        System.out.println("Clicked changeUsername");
+
+        if (!inputNewUsername.getText().isEmpty()) {
+            // Replace with your logic for notifying the change in username
+            // ConnectedUser currentUser = new ConnectedUser(LocalDatabase.Database.currentUser, LocalDatabase.Database.currentIP);
+            // nwm.notifyChangeUsername(currentUser, inputNewUsername.getText());
+
+            ConnectedUser currentUser = new ConnectedUser(LocalDatabase.Database.currentUser, LocalDatabase.Database.currentIP);
+            nwm.notifyChangeUsername(currentUser,inputNewUsername.getText());
+            changedUsernameLabel.setText("Username changed!");
+            changedUsernameLabel.setText("Username changed!");
+        } else {
+            System.out.println("Empty textfield new username");
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new SecondWindow().start();
+        });
+    }
+}
+
+
+
+
+/*
 import com.insa.database.LocalDatabase;
 import com.insa.network.ConnectedUser;
 import com.insa.network.NetworkManager;
@@ -74,3 +156,4 @@ public class SecondWindow {
 
     }
 }
+*/
